7b7c44e5496544122c4d01623bb13a96bd965a86
Eliezer1109 <eliezerdasilva503@gmail.com>
2024-05-13 23:39:46 -0300

    ✨ feat: Criação das funções

---
+++ index.js
@@ -0,0 +1,33 @@
+function abreAdiContato(){
+    window.location ='index_02.html';
+
+}
+
+function fechaAdiContato(){
+    window.location ='index.html'
+
+}
+
+function abrirEditContato(){
+    window.location ='index_02.html';
+}
+
+function fechaEdita(){
+    window.location ='index.html'
+}
+
+function excluirContato() {
+    const nameValue = document.getElementById("nome").value(" ");
+
+}
+
+function adicionaContatos(){
+    const nameValue = document.getElementById("nome").value;
+    const emailValue = document.getElementById("email").value;
+
+    console.log(nameValue)
+    console.log(emailValue)
+
+
+}
+
