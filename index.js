f4ea48836426c2c558a285c9263cf0c1a18fa592
Eliezer1109 <eliezerdasilva503@gmail.com>
2024-06-04 19:37:30 -0300

    ♻️ Refactor: alterando a funcao abrir aba para pegar os valores para edicao e refatorando a funcao excluirContato.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function abrirAba(nome){
-    const elemento =  document.querySelector(nome)
-    elemento.style.display="flex";
 
+let id_edicao = "";
+
+function abrirAba(nome,id){
+    const elementoParaAbertura =  document.querySelector(nome)
+    elementoParaAbertura.style.display="flex";
+
+    const txt1 = document.getElementById("txt01").textContent
+    const txt2 = document.getElementById("txt02").textContent    // text content pega o texto de dentro da tag HTML de texto, 
+    const txt3 = document.getElementById("txt03").textContent    //  EX: <p> Rendimento </p> pega palavra rendimento 
+
+    console.log(txt1,txt2,txt3)
+
+    const elementoInputNome = document.getElementById("nomeDeEdicao")
+    const elementoInputEmail = document.getElementById("emailDeEdicao")
+    // const elementoInputNome = document.getElementById("nomeDeEdicao")
+
+    elementoInputNome.value = txt1
+    elementoInputEmail.value = txt2
+    
+    id_edicao = id 
+   
 }
 
 function fecharAba(nome){
@@ -9,9 +27,10 @@ function fecharAba(nome){
     elemento.style.display="none";
 }
 
-function excluirContato(nome) {
-    const name = document.getElementById(nome);
-    name.remove()
+function excluirContato(id) {
+    const elementoHTML = document.getElementById(id);
+    elementoHTML.remove()
+
 }
 
 function limparInput(){
@@ -23,22 +42,11 @@ function adicionaContatos() {
     const emailValue = document.getElementById("email").value;
     
     const contatos = document.getElementById("contatos_011");
-
-    console.log(nameValue)
-    console.log(emailValue)
-
-
-        // problema do id : preciso pegar o numero do id do ultimo elemento de contato colocado na tela
-        // e somar 1 nele. esse sera meu novo id
-        // como pegar o ultimo filho de um pai : Lst child  
-        // https://developer.mozilla.org/pt-BR/docs/Web/API/Node/lastChild
-        // https://developer.mozilla.org/pt-BR/docs/Web/CSS/:last-child
-
-        let numerodoId = 2 + i;
-        let id = 'contato_01' + numerodoId;
+    let ultimoId = parseInt(contatos.lastElementChild?.id) || 0
+    const somaId = ultimoId + 1;
     
         contatos.innerHTML += `
-        <div class="contato" id="contato_01">
+        <div class="contato" id="${somaId}">
         <div class="dados_cadastrados">
             <p>${nameValue}</p>
             <p>email:${emailValue}</p>
@@ -46,7 +54,7 @@ function adicionaContatos() {
                  
         </div>
         <div class="botoes">
-            <div class="icone" onclick="abrirEditContato()">
+            <div class="icone" onclick="abrirAba('.pagina_de_edicao')">
                 <svg fill="#676767" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M432.4 787.2c-24.1 27.6-34.7 34.2-68.4 46.6-51.9 19.4-147 55.1-208.9 78.3-11.7 4.6-55.9 2.3-37.4-41.1 21-60.4 51.8-149.6 69.4-200.5 12.8-35.9 17.9-45.2 43.7-69.3l266.5-266.5L686 523.4C686 523.4 506.6 710.1 432.4 787.2z" />
@@ -55,7 +63,7 @@ function adicionaContatos() {
                         d="M888.3 321.2l-80.8 80.9L618.8 213.4l80.9-80.8c29.8-29.8 78-29.8 107.8 0l80.9 80.8C918.1 243.1 918.1 291.4 888.3 321.2z" />
                 </svg>
             </div>
-            <div class="icone" onclick="excluirContato(${id})">
+            <div class="icone" onclick="excluirContato('${somaId}')">
                 <svg fill="#676767" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" />
@@ -67,5 +75,13 @@ function adicionaContatos() {
         `;
 }    
 
+function EditarContato(){
+    
+    const contato = document.getElementById(id_edicao)
+    console.log(contato);
+    
+    //pegar inputs do html de edicao
+    //pegar elementos de html dos dados que estao no contato
+    //escrever novos dados de contato e colocar no lugar dos velhos   
 
-
+}
