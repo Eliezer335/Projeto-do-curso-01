1a2c1cac5c693dec2a1e9f7d29c361e6e5c1f6cb
Eliezer1109 <eliezerdasilva503@gmail.com>
2024-06-05 21:35:23 -0300

    ♻️ Refactor: fazendo o botao de edicao funcionar

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 
 let id_edicao = "";
+let txt01_edicao = "";
+let txt02_edicao = "";
+let txt03_edicao = "";
 
-function abrirAba(nome,id){
+function abrirAba(nome,id,txt01,txt02,txt03){
     const elementoParaAbertura =  document.querySelector(nome)
     elementoParaAbertura.style.display="flex";
 
-    const txt1 = document.getElementById("txt01").textContent
-    const txt2 = document.getElementById("txt02").textContent    // text content pega o texto de dentro da tag HTML de texto, 
-    const txt3 = document.getElementById("txt03").textContent    //  EX: <p> Rendimento </p> pega palavra rendimento 
+    const txt1 = document.getElementById(txt01 || "txt01")?.textContent  
+    const txt2 = document.getElementById(txt02 || "txt02")?.textContent   // text content pega o texto de dentro da tag HTML de texto, 
+    const txt3 = document.getElementById(txt03 || "txt03")?.textContent  //  EX: <p> Rendimento </p> pega palavra rendimento 
 
     console.log(txt1,txt2,txt3)
 
@@ -20,6 +23,9 @@ function abrirAba(nome,id){
     elementoInputTel.value = txt3
 
     id_edicao = id 
+    txt01_edicao = txt01
+    txt02_edicao = txt02
+    txt03_edicao = txt03
    
 }
 
@@ -38,7 +44,7 @@ function limparInput(){
 
 }
 
-function adicionaContatos() {
+function adicionarContatos() {
     const nameValue = document.getElementById("nome").value;
     const emailValue = document.getElementById("email").value;
     const telValue = document.getElementById("tel").value;
@@ -46,17 +52,17 @@ function adicionaContatos() {
     const contatos = document.getElementById("contatos_011");
     let ultimoId = parseInt(contatos.lastElementChild?.id) || 0
     const somaId = ultimoId + 1;
-    
+    console.log(`"txt${somaId}1"`)
         contatos.innerHTML += `
         <div class="contato" id="${somaId}">
         <div class="dados_cadastrados">
-            <p>${nameValue}</p>
-            <p>email:${emailValue}</p>
-            <p>tel.:${telValue}</p>
+            <p id="txt${somaId}1">${nameValue}</p>
+            <p id="txt${somaId}2">email:${emailValue}</p>
+            <p id="txt${somaId}3">tel.:${telValue}</p>
                  
         </div>
         <div class="botoes">
-            <div class="icone" onclick="abrirAba('.pagina_de_edicao')">
+            <div class="icone" onclick="abrirAba('.pagina_de_edicao',${somaId},'txt${somaId}1','txt${somaId}2','txt${somaId}3')">
                 <svg fill="#676767" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M432.4 787.2c-24.1 27.6-34.7 34.2-68.4 46.6-51.9 19.4-147 55.1-208.9 78.3-11.7 4.6-55.9 2.3-37.4-41.1 21-60.4 51.8-149.6 69.4-200.5 12.8-35.9 17.9-45.2 43.7-69.3l266.5-266.5L686 523.4C686 523.4 506.6 710.1 432.4 787.2z" />
@@ -80,27 +86,21 @@ function adicionaContatos() {
 function EditarContato(){
     
     const contato = document.getElementById(id_edicao)
-   // console.log(contato);
 
     const elementoEditaNome = document.getElementById("nomeDeEdicao").value
     const elementoEditaEmail = document.getElementById("emailDeEdicao").value
     const elementoEditaTel = document.getElementById("telDeEdicao").value
-    const contatos = document.getElementById("contatos_011");
 
-    //console.log(elementoEditaNome,elementoEditaEmail);
-    let ultimoId = parseInt(contatos.lastElementChild?.id) || 0
-    const somaId = ultimoId + 1;
-
-    contatos.innerHTML = `
-        <div class="contato" id="">
+    contato.innerHTML = `
+        
         <div class="dados_cadastrados">
-            <p>Nome:${elementoEditaNome}</p>
-            <p>email:${elementoEditaEmail}</p>
-            <p>tel.:${elementoEditaTel}</p>               
+            <p id=${txt01_edicao}>Nome:${elementoEditaNome}</p>
+            <p id=${txt02_edicao}>email:${elementoEditaEmail}</p>
+            <p id=${txt03_edicao}>tel.:${elementoEditaTel}</p>               
         </div>
         <div class="botoes">
 
-        <div class="icone" onclick="abrirAba('.pagina_de_edicao','0')">
+        <div class="icone" onclick="abrirAba('.pagina_de_edicao','${id_edicao}','${txt01_edicao}','${txt02_edicao}','${txt03_edicao}')">
             <svg fill="#676767" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M432.4 787.2c-24.1 27.6-34.7 34.2-68.4 46.6-51.9 19.4-147 55.1-208.9 78.3-11.7 4.6-55.9 2.3-37.4-41.1 21-60.4 51.8-149.6 69.4-200.5 12.8-35.9 17.9-45.2 43.7-69.3l266.5-266.5L686 523.4C686 523.4 506.6 710.1 432.4 787.2z" />
@@ -110,13 +110,13 @@ function EditarContato(){
             </svg>
         </div>
 
-        <div class="icone" onclick="excluirContato('${somaId}')">
+        <div class="icone" onclick="excluirContato('${id_edicao}')">
             <svg fill="#676767" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" />
             </svg>
         </div>
-
+          
         `;
   
 
