5ac731b4207dd8a12e475e2c559c501d80f766bc
Eliezer1109 <eliezerdasilva503@gmail.com>
2024-07-25 22:26:15 -0300

    ✨ feat: criando a função do localStorage para que os contato fiquem salvos na tela mesmo que de o reload

--- index.js
+++ index.js
@@ -56,6 +56,12 @@ function excluirContato(id) {
     const elementoHTML = document.getElementById(id);
     elementoHTML.remove()
 
+    //excluir o elemento tambem do localstorage 
+    //1 entrar na lista procurando um elemento com id igual o que ta na função
+    //se for igual exclui se nao 
+    //procurar a posição do elemento 
+    // usar o metodo filter
+
 }
 
 function limparInput(){
@@ -77,9 +83,15 @@ function adicionarContatos() {
         email: document.getElementById("email").value,
         tel: document.getElementById("tel").value
     }
+    const contatosRegistrados = JSON.parse(localStorage.getItem("contatosObj"))
+    if(contatosRegistrados){
+        contatosObj= contatosRegistrados;
+    }
     contatosObj.push(novoContato);
+    console.log(contatosObj);
     const objetoString = JSON.stringify(contatosObj)
     localStorage.setItem("contatosObj",objetoString)
+
 
         contatos.innerHTML += `
         <div class="contato" id="${somaId}">
@@ -110,7 +122,50 @@ function adicionarContatos() {
         
         `;
         
-}    
+}
+
+function adicionarDoLocalStorage(){
+    const contatosRegistrados = JSON.parse(localStorage.getItem("contatosObj"))
+
+    if(contatosRegistrados){
+       const contatos = document.getElementById("contatos_011");
+
+       for (const contato of contatosRegistrados) {
+    
+            contatos.innerHTML += `
+            <div class="contato" id="${contato.id}">
+            <div class="dados_cadastrados">
+                <p id="txt${contato.id}1">nome: ${contato.nome}</p>
+                <p id="txt${contato.id}2">email: ${contato.email}</p>
+                <p id="txt${contato.id}3">tel.: ${contato.tel}</p>
+                     
+            </div>
+            <div class="botoes">
+                <div class="icone" onclick="abrirAba('.pagina_de_edicao',${contato.id},'txt${contato.id}1','txt${contato.id}2','txt${contato.id}3')">
+                    <svg fill="#676767" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M432.4 787.2c-24.1 27.6-34.7 34.2-68.4 46.6-51.9 19.4-147 55.1-208.9 78.3-11.7 4.6-55.9 2.3-37.4-41.1 21-60.4 51.8-149.6 69.4-200.5 12.8-35.9 17.9-45.2 43.7-69.3l266.5-266.5L686 523.4C686 523.4 506.6 710.1 432.4 787.2z" />
+                        <path d="M726.5 482.9 537.9 294.3 578.3 253.8 767 442.5Z" />
+                        <path
+                            d="M888.3 321.2l-80.8 80.9L618.8 213.4l80.9-80.8c29.8-29.8 78-29.8 107.8 0l80.9 80.8C918.1 243.1 918.1 291.4 888.3 321.2z" />
+                    </svg>
+                </div>
+                <div class="icone" onclick="excluirContato('${contato.id}')">
+                    <svg fill="#676767" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" />
+                    </svg>
+                </div>
+            </div>  
+            </div>
+            
+            `;
+        
+       }
+    }
+
+
+}
 
 function EditarContato(){
     
@@ -167,6 +222,7 @@ function pesquisarContatos() {
 }
 
 pesquisarContatos();
+adicionarDoLocalStorage();
 
 
 
